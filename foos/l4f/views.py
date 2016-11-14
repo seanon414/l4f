9c2d21f874df0272513317d9e98f193d8426875f
Sean O'Neill <sean.o'neill@dealertrack.com>
2016-11-14 17:13:32 +0000

    Using env var to store slackbot token

--- foos/l4f/views.py
+++ foos/l4f/views.py
@@ -1,12 +1,15 @@
+import os
+
 from django.http import HttpResponse
 
 from slacker import Slacker
 
-slack = Slacker('* slack token here*')
+slack_key = os.environ['SLACK_BOT']
+slack = Slacker(slack_key)
 channel = '#l4f'
 
 
 def index(request):
     slack_string = 'This is a test... 123...'
     slack.chat.post_message(channel, slack_string)
-    return HttpResponse("Put foose stuff here")
+    return HttpResponse('')
