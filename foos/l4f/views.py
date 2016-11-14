9860ad7be3a986c098f495420694c44f08cf2b55
Sean O'Neill <sean.o'neill@dealertrack.com>
2016-11-14 16:57:19 +0000

    Adding slack integration with django app

--- foos/l4f/views.py
+++ foos/l4f/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from slacker import Slacker
+
+slack = Slacker('xoxb-35700221924-XjG9qR4swYgOf4uEIWy1F7JG')
+channel = '#l4f'
+
 
 def index(request):
+    slack_string = 'This is a test... 123...'
+    slack.chat.post_message(channel, slack_string)
     return HttpResponse("Put foose stuff here")
