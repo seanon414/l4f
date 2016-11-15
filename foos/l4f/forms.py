f7d56f66ad8071b933b454469c615c3f43072833
Irene Shih <irene.shih@dealertrack.com>
2016-11-15 14:28:37 -0500

    remove min and max values

--- foos/l4f/forms.py
+++ foos/l4f/forms.py
@@ -4,9 +4,7 @@ from django.forms.fields import IntegerField
 
 class HomePageForm(forms.Form):
     number_of_players = IntegerField(required=False,
-                                     initial=1,
-                                     min_value=1,
-                                     max_value=3)
+                                     initial=1)
 
     def __init__(self, *args, **kwargs):
         super(HomePageForm, self).__init__(*args, **kwargs)
